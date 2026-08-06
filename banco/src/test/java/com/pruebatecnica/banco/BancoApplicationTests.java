ddc7809f7566a6c4255989272430558acf357e4c
Cristian Tafur <cftd0331@gmail.com>
2026-08-05 20:48:49 -0500

    test(config): ejecutar las pruebas sobre H2 en memoria sin depender de Docker

--- banco/src/test/java/com/pruebatecnica/banco/BancoApplicationTests.java
+++ banco/src/test/java/com/pruebatecnica/banco/BancoApplicationTests.java
@@ -2,12 +2,17 @@ package com.pruebatecnica.banco;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * Prueba de humo: verifica que todo el contexto de Spring se construya.
+ * Usa el perfil "test" (H2 en memoria) para no depender del contenedor de PostgreSQL.
+ */
 @SpringBootTest
+@ActiveProfiles("test")
 class BancoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
